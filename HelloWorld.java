7c3212e199dd58339b19c61e5506caf4fb3dc9b2
priyajaya <49052503+priyajaya@users.noreply.github.com>
2019-07-03 12:16:23 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+import java.io.*;
+
+public class HelloWorld
+{
+
+ 
+    public static void main(String []args)
+ 
+    {
+      
+Scanner s=new Scanner(System.in);
+     
+ String s1=s.nextLine();
+     
+ int s2=Integer.ParseInt(s1);
+     
+ for(int i=1;i<s2;i++)
+      
+String s3=s2.charAt(i);
+      
+int l=s2.length();
+      
+System.out.println(l);
+     
+ }
+   
+ 
+}
